3449a414539fc46e163bc5809a9d9c1b0bc4205c
Adrian Rollett <acrollet@gmail.com>
2019-05-16 16:50:31 -0600

    Add tests confirming that OD classes are available
    OD-280

--- tests/Feature/OdTest.php
+++ tests/Feature/OdTest.php
@@ -2,8 +2,17 @@
 
 namespace Tests\Feature;
 
-use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use OpenDialogAi\ActionEngine\Service\ActionEngineInterface;
+use OpenDialogAi\ContextEngine\AttributeResolver\AttributeResolver;
+use OpenDialogAi\ContextEngine\ContextManager\ContextService;
+use OpenDialogAi\ContextEngine\Contexts\User\UserService;
+use OpenDialogAi\ConversationEngine\ConversationStore\ConversationStoreInterface;
+use OpenDialogAi\InterpreterEngine\Service\InterpreterServiceInterface;
+use OpenDialogAi\ResponseEngine\Service\ResponseEngineServiceInterface;
+use OpenDialogAi\SensorEngine\Service\SensorService;
+use OpenDialogAi\Webchat\Console\Commands\WebchatSettings;
+use Tests\TestCase;
 
 class OdTest extends TestCase
 {
@@ -54,5 +63,46 @@ class OdTest extends TestCase
             'teamName' => 'OpenDialog Webchat',
             'useAvatars' => true,
         ]);
+    }
+
+    /**
+     * Verify that the OD-Core service providers are available.
+     *
+     * @return void
+     */
+    public function testOdCoreServiceProviders()
+    {
+        $actionEngine = app('OpenDialogAi\ActionEngine\Service\ActionEngineInterface');
+        $this->assertInstanceOf(ActionEngineInterface::class, $actionEngine);
+
+        $contextService = app('OpenDialogAi\ContextEngine\ContextManager\ContextService');
+        $this->assertInstanceOf(ContextService::class, $contextService);
+        $attributeResolver = app('OpenDialogAi\ContextEngine\AttributeResolver\AttributeResolver');
+        $this->assertInstanceOf(AttributeResolver::class, $attributeResolver);
+        $userService = app('OpenDialogAi\ContextEngine\Contexts\User\UserService');
+        $this->assertInstanceOf(UserService::class, $userService);
+
+        $conversationStore = app('OpenDialogAi\ConversationEngine\ConversationStore\ConversationStoreInterface');
+        $this->assertInstanceOf(ConversationStoreInterface::class, $conversationStore);
+
+        $interpreterService = app('OpenDialogAi\InterpreterEngine\Service\InterpreterServiceInterface');
+        $this->assertInstanceOf(InterpreterServiceInterface::class, $interpreterService);
+
+        $responseEngineService = app('OpenDialogAi\ResponseEngine\Service\ResponseEngineServiceInterface');
+        $this->assertInstanceOf(ResponseEngineServiceInterface::class, $responseEngineService);
+
+        $sensorService = app('OpenDialogAi\SensorEngine\Service\SensorService');
+        $this->assertInstanceOf(SensorService::class, $sensorService);
+    }
+
+    /**
+     * Verify that the OD-Webchat service provider is available.
+     *
+     * @return void
+     */
+    public function testOdWebchatServiceProvider()
+    {
+        $webChatSettings = app('OpenDialogAi\Webchat\Console\Commands\WebchatSettings');
+        $this->assertInstanceOf(WebchatSettings::class, $webChatSettings);
     }
 }
