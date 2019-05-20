a080d28f78ec28eb4f532d839c2f0ef4d5757e9e
Stuart Haigh <stuarth@greenshootlabs.com>
2019-05-20 14:52:37 +0100

    commits the interpreter engine config file to set up the WELCOME callback

---
+++ config/opendialog/interpreter_engine.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    /**
+     * A registration of know LUIS entities mapped to known attribute type. If an entity is returned from LUIS that is
+     * not an already registered attribute name and is not mapped here, a StringAttribute will be used
+     *
+     * Mapping is {luis_entity_type} => {OD_attribute_name}
+     */
+    'luis_entities' => [
+//         'example_type' => 'first_name'
+    ],
+
+    /**
+     * Custom interpreters registered in the format
+     */
+    'custom_interpreters' => [
+//    \OpenDialogAi\InterpreterEngine\tests\Interpreters\DummyInterpreter::class
+    ],
+
+    'default_interpreter' => 'interpreter.core.callbackInterpreter',
+
+    /**
+     * List of supported intents in the format 'callback_id' => 'intent_name'
+     */
+    'supported_callbacks' => [
+        'WELCOME' => 'intent.core.welcome',
+    ]
+];
