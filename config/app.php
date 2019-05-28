b78652df26c0b8923707f304786ba2cfb55cc834
Adrian Rollett <acrollet@gmail.com>
2019-05-28 16:01:44 -0600

    Install adminarchitect

--- config/app.php
+++ config/app.php
@@ -175,7 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\NovaServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        Terranet\Administrator\ServiceProvider::class,
     ],
 
     /*
