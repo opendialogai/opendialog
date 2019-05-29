a98ee6b655bae15f605ac436f6246647052a1a8b
Adrian Rollett <acrollet@gmail.com>
2019-05-29 17:08:40 -0600

    Fix coding standards
    OD-303

--- app/Http/Terranet/Administrator/Modules/Conversations.php
+++ app/Http/Terranet/Administrator/Modules/Conversations.php
@@ -2,14 +2,6 @@
 
 namespace App\Http\Terranet\Administrator\Modules;
 
-use League\CommonMark\Block\Element\FencedCode;
-use League\CommonMark\Block\Element\IndentedCode;
-use League\CommonMark\CommonMarkConverter;
-use League\CommonMark\Environment;
-use OpenDialogAi\ResponseEngine\OutgoingIntent;
-use Spatie\CommonMarkHighlighter\FencedCodeRenderer;
-use Spatie\CommonMarkHighlighter\IndentedCodeRenderer;
-use Symfony\Component\Yaml\Yaml;
 use Terranet\Administrator\Columns\Element;
 use Terranet\Administrator\Contracts\Module\Editable;
 use Terranet\Administrator\Contracts\Module\Exportable;
@@ -42,7 +34,7 @@ class Conversations extends Scaffolding implements Navigable, Filtrable, Editabl
      *
      * @var string
      */
-    protected $model = 'App\Http\Terranet\Administrator\Modules\Conversation';
+    protected $model = 'App\Http\Terranet\Administrator\Presentable\Conversation';
 
     public function linkAttributes()
     {
@@ -117,78 +109,5 @@ class Conversations extends Scaffolding implements Navigable, Filtrable, Editabl
         return $this->scaffoldWidgets()
             ->push(new RevisionViewer($conversation))
             ->push(new StateLogs($conversation));
-    }
-}
-
-class Conversation extends \OpenDialogAi\ConversationBuilder\Conversation implements \Terranet\Presentable\PresentableInterface
-{
-    use \Terranet\Presentable\PresentableTrait;
-
-    protected $fillable = [
-        'id',
-        'name',
-        'model',
-        'notes',
-        'status',
-        'yaml_validation_status',
-        'yaml_schema_validation_status',
-        'scenes_validation_status',
-        'model_validation_status',
-        'outgoingIntents',
-    ];
-
-    protected $presenter = ConversationPresenter::class;
-}
-
-class ConversationPresenter extends \Terranet\Presentable\Presenter
-{
-    public function title()
-    {
-        return link_to_route('scaffold.view', $this->presentable->name, [
-            'module' => 'conversations',
-            'id' => $this->presentable
-        ]);
-    }
-
-    public function model()
-    {
-        $environment = Environment::createCommonMarkEnvironment();
-        $environment->addBlockRenderer(FencedCode::class, new FencedCodeRenderer(['yaml']));
-        $environment->addBlockRenderer(IndentedCode::class, new IndentedCodeRenderer(['yaml']));
-
-        $commonMarkConverter = new CommonMarkConverter([], $environment);
-
-        return $commonMarkConverter->convertToHtml("```\n" . $this->presentable->model . "\n```");
-    }
-
-    public function outgoingIntents()
-    {
-        $output = '';
-
-        $yaml = Yaml::parse($this->presentable->model)['conversation'];
-
-        foreach ($yaml['scenes'] as $sceneId => $scene) {
-            foreach ($scene['intents'] as $intent) {
-                foreach ($intent as $tag => $value) {
-                    if ($tag == 'b') {
-                        foreach ($value as $key => $intent) {
-                            if ($key == 'i') {
-                                $outgoingIntent = OutgoingIntent::where('name', $intent)->first();
-
-                                if ($outgoingIntent) {
-                                    $output .= '<div><a href="/cms/outgoing_intents/' . $outgoingIntent->id . '">' . $intent . '</a></div>';
-                                } else {
-                                    $output .= '<div><a href="/cms/outgoing_intents/new">' . $intent . '</a></div>';
-                                }
-                                break;
-                            }
-                        }
-                        break;
-                    }
-                }
-            }
-        }
-
-        return $output;
     }
 }
---
+++ app/Http/Terranet/Administrator/Presentable/ConversationPresenter.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Terranet\Administrator\Presentable;
+
+use League\CommonMark\Block\Element\FencedCode;
+use League\CommonMark\Block\Element\IndentedCode;
+use League\CommonMark\CommonMarkConverter;
+use League\CommonMark\Environment;
+use OpenDialogAi\ResponseEngine\OutgoingIntent;
+use Spatie\CommonMarkHighlighter\FencedCodeRenderer;
+use Spatie\CommonMarkHighlighter\IndentedCodeRenderer;
+use Symfony\Component\Yaml\Yaml;
+use Terranet\Presentable\Presenter;
+
+class ConversationPresenter extends Presenter
+{
+    public function title()
+    {
+        return link_to_route('scaffold.view', $this->presentable->name, [
+            'module' => 'conversations',
+            'id' => $this->presentable
+        ]);
+    }
+
+    public function model()
+    {
+        $environment = Environment::createCommonMarkEnvironment();
+        $environment->addBlockRenderer(FencedCode::class, new FencedCodeRenderer(['yaml']));
+        $environment->addBlockRenderer(IndentedCode::class, new IndentedCodeRenderer(['yaml']));
+
+        $commonMarkConverter = new CommonMarkConverter([], $environment);
+
+        return $commonMarkConverter->convertToHtml("```\n" . $this->presentable->model . "\n```");
+    }
+
+    public function outgoingIntents()
+    {
+        $output = '';
+
+        $yaml = Yaml::parse($this->presentable->model)['conversation'];
+
+        foreach ($yaml['scenes'] as $sceneId => $scene) {
+            foreach ($scene['intents'] as $intent) {
+                foreach ($intent as $tag => $value) {
+                    if ($tag == 'b') {
+                        foreach ($value as $key => $intent) {
+                            if ($key == 'i') {
+                                $outgoingIntent = OutgoingIntent::where('name', $intent)->first();
+
+                                if ($outgoingIntent) {
+                                    $output .= '<div><a href="/cms/outgoing_intents/' . $outgoingIntent->id . '">' . $intent . '</a></div>';
+                                } else {
+                                    $output .= '<div><a href="/cms/outgoing_intents/new">' . $intent . '</a></div>';
+                                }
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+        return $output;
+    }
+}
