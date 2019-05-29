14e78e982a3ef4a702c686a44e770a03ff01f255
Adrian Rollett <acrollet@gmail.com>
2019-05-29 17:37:41 -0600

    Update setup script for adminarchitect
    OD-303

--- scripts/set_up_od.sh
+++ scripts/set_up_od.sh
@@ -6,11 +6,6 @@ trap errormsg EXIT
 # Ensure that lando is installed.
 command -v lando >/dev/null 2>&1 || { echo >&2 "Please install lando: https://docs.devwithlando.io/installation/system-requirements.html"; exit 1; }
 
-# Check for the nova auth file.
-if [ ! -f auth.json ]; then
-    echo >&2 "Please configure the auth.json file for Nova."; exit 1;
-fi
-
 echo "Adding Laravel environment settings..."
 cp -n .env.example.lando .env || echo "A .env file was already present, not copying example..."
 
@@ -18,7 +13,7 @@ echo "Starting services..."
 lando start
 
 echo "Installing dependencies..."
-lando composer install
+composer install
 
 echo "Setting up the webchat widget..."
 bash update-web-chat.sh
@@ -33,15 +28,14 @@ echo "Application level config files"
 php artisan vendor:publish --tag=od-config
 
 echo "Creating example conversations..."
-lando artisan conversations:setup --yes
+lando artisan conversations:setup
 
-echo "Setting up Nova..."
-lando artisan nova:publish
-CURDATE=$( date -u +"%Y-%m-%d %H:%M:%S" )
+echo "Setting up the admin interface..."
+npm run dev
 lando ssh --service database --command 'mysql -uroot laravel -e '"'"'INSERT INTO users (name, email, password, created_at, updated_at) VALUES ("admin", "admin@example.com", "$2y$10$BEhBWA12KObSY9Ua2G0VeOg2hWMT1GIa8huHD83HCEHnJLnRcH8w6", NOW(), NOW())'"'"' '
 
 echo
-echo "The admin console is available here: https://opendialog.lndo.site/admin"
+echo "The admin console is available here: https://opendialog.lndo.site/cms"
 echo "You may login with the credentials admin@example.com / opendialog"
 echo
 echo "Finished! Now you may go to: https://opendialog.lndo.site/demo"
