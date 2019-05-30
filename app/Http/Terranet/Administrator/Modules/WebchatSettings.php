966262522ea22983fb3f85497b778ac49c2c8977
Adrian Rollett <acrollet@gmail.com>
2019-05-30 11:06:49 -0600

    Add webchat settings resource
    OD-303

---
+++ app/Http/Terranet/Administrator/Modules/WebchatSettings.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Terranet\Administrator\Modules;
+
+use Terranet\Administrator\Columns\Element;
+use Terranet\Administrator\Contracts\Module\Editable;
+use Terranet\Administrator\Contracts\Module\Exportable;
+use Terranet\Administrator\Contracts\Module\Filtrable;
+use Terranet\Administrator\Contracts\Module\Navigable;
+use Terranet\Administrator\Contracts\Module\Sortable;
+use Terranet\Administrator\Contracts\Module\Validable;
+use Terranet\Administrator\Filters\Scope;
+use Terranet\Administrator\Form\Type\Hidden;
+use Terranet\Administrator\Scaffolding;
+use Terranet\Administrator\Traits\Module\AllowFormats;
+use Terranet\Administrator\Traits\Module\AllowsNavigation;
+use Terranet\Administrator\Traits\Module\HasFilters;
+use Terranet\Administrator\Traits\Module\HasForm;
+use Terranet\Administrator\Traits\Module\HasSortable;
+use Terranet\Administrator\Traits\Module\ValidatesForm;
+
+/**
+ * Administrator Resource ChatbotUser
+ *
+ * @package Terranet\Administrator
+ */
+class WebchatSettings extends Scaffolding implements Navigable, Filtrable, Editable, Validable, Sortable, Exportable
+{
+    use HasFilters, HasForm, HasSortable, ValidatesForm, AllowFormats, AllowsNavigation;
+
+    /**
+     * The module Eloquent model
+     *
+     * @var string
+     */
+    protected $model = 'App\Http\Terranet\Administrator\Presentable\WebchatSetting';
+
+    public function linkAttributes()
+    {
+        return ['icon' => 'fa fa-pencil'];
+    }
+
+    public function columns()
+    {
+        return $this->scaffoldColumns()->without(['id']);
+    }
+
+    public function form()
+    {
+        $form = $this->scaffoldForm();
+
+        return $form;
+    }
+}
---
+++ app/Http/Terranet/Administrator/Presentable/WebchatSetting.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Terranet\Administrator\Presentable;
+
+use App\Http\Terranet\Administrator\Presentable\WebchatSettingPresenter;
+use OpenDialogAi\Webchat\WebchatSetting as OdWebchatSetting;
+use Terranet\Presentable\PresentableInterface;
+use Terranet\Presentable\PresentableTrait;
+
+class WebchatSetting extends OdWebchatSetting implements PresentableInterface
+{
+    use PresentableTrait;
+
+    protected $fillable = [
+        'name',
+        'value',
+    ];
+
+    protected $presenter = WebchatSettingPresenter::class;
+}
