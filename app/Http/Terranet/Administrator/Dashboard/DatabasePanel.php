243814027db30717af6e767ea2a36f87c873cd79
Nicolò Caruso <nicolo@istos.it>
2019-07-01 12:27:39 +0200

    OPNDLG-5: Remove AdminArchitect

--- app/Http/Terranet/Administrator/Dashboard/DatabasePanel.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Http\Terranet\Administrator\Dashboard;
-
-use Terranet\Administrator\Dashboard\Panels\DatabasePanel as CoreDatabasePanel;
-
-class DatabasePanel extends CoreDatabasePanel
-{
-}
--- resources/assets/administrator/js/editors/markdown.js
+++
@@ -1,5 +0,0 @@
-const SimpleMDE = require('simplemde');
-
-document.querySelectorAll('[data-editor="markdown"]').forEach((e) => {
-    new SimpleMDE({element: e});
-});
