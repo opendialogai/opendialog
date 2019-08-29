9c735e18e20a261d3b97f617d9a81ed548d28105
Nicolò Caruso <nicolo@istos.it>
2019-08-29 10:42:25 +0200

    Make Dashboard cards configurable

---
+++ config/admin-stats.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+
+    'cards' => [
+        [
+            [
+                'type' => 'line-chart',
+                'name' => 'Users',
+                'endpoint' => '/stats/users',
+                'width' => 2,
+            ],
+            [
+                'type' => 'line-chart',
+                'name' => 'Users',
+                'endpoint' => '/stats/users',
+                'width' => 2,
+            ],
+        ],
+        [
+            [
+                'type' => 'single-number',
+                'name' => 'Cost',
+                'endpoint' => '/stats/cost',
+                'width' => 3,
+            ],
+            [
+                'type' => 'single-number',
+                'name' => 'Cost',
+                'endpoint' => '/stats/cost',
+                'width' => 3,
+            ],
+            [
+                'type' => 'single-number',
+                'name' => 'Cost',
+                'endpoint' => '/stats/cost',
+                'width' => 3,
+            ],
+        ],
+    ],
+
+];
