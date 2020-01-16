33d169ea7a8898edb343cbbb23a6410db91a30c6
Duncan <duncanuk@gmail.com>
2020-01-16 13:12:47 +0000

    adds multiple repo support

--- scripts/setup-local-core.sh
+++ scripts/setup-local-core.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
 LOCAL_VENDOR_DIR="vendor-local"
-LOCAL_CORE_DIR="vendor-local/opendialog-core"
+
 if [ ! -d "./$LOCAL_VENDOR_DIR" ]; then
   echo "Creating local vendor directory"
   mkdir "./$LOCAL_VENDOR_DIR"
 fi
-if [ ! -d "./$LOCAL_CORE_DIR" ]; then
-  echo "CORE::: Cloning opendialog-core to vendor-local"
-  git clone git@github.com:opendialogai/core.git vendor-local/opendialog-core
-  cd $LOCAL_CORE_DIR
-  echo "CORE::: Installing composer"
-  composer install
-  cd ../..
-fi
+
+for repo in opendialog-core opendialog-webchat
+do
+  LOCAL_DIR="vendor-local/$repo"
+
+  if [ ! -d "./$LOCAL_DIR" ]; then
+    subrepo="$( cut -d '-' -f 2- <<< "$repo" )";
+    echo "$repo::: Cloning $repo to vendor-local"
+    git clone git@github.com:opendialogai/$subrepo.git vendor-local/$repo
+    cd $LOCAL_DIR
+    echo "$repo::: Installing composer"
+    composer install
+    cd ../..
+  fi
+done
+
 echo "OPENDIALOG::: Installing composer with symlink"
 export COMPOSER=composer-dev.json
 composer update
