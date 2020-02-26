9253fc9f5e4e9011ce7f73810a1b053d7309aa7a
Nicolò Caruso <nicolo@istos.it>
2020-02-26 11:20:14 +0100

    Allow to preview a “Full-page rich message”

--- resources/js/views/ConversationLog.vue
+++ resources/js/views/ConversationLog.vue
@@ -37,7 +37,7 @@
               </template>
             </div>
           </template>
-          <template v-if="message.type == 'rich'">
+          <template v-if="message.type == 'rich' || v-if="message.type == 'fp-rich'">
             <div class="rich-message">
               <div class="rich-message--title mb-1" v-if="message.data.title">{{ message.data.title }}</div>
               <div class="rich-message--subtitle mb-2" v-if="message.data.subtitle">{{ message.data.subtitle }}</div>
