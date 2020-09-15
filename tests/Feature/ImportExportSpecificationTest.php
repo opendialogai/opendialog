f40651e64988a7c1a56623a257523a07dd7a7466
Nicolò Caruso <nicolo@istos.it>
2020-09-15 11:26:04 +0200

    Add tests for import/export specification

---
+++ tests/Feature/ImportExportSpecificationTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Support\Facades\Artisan;
+use OpenDialogAi\ConversationBuilder\Conversation;
+use Tests\TestCase;
+
+class ImportExportSpecificationTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Artisan::call(
+            'schema:init',
+            [
+                '--yes' => true
+            ]
+        );
+    }
+
+    public function testImportSpecification()
+    {
+        $this->assertDatabaseMissing('conversations', ['name' => 'no_match_conversation']);
+        $this->assertDatabaseMissing('outgoing_intents', ['name' => 'intent.core.NoMatchResponse']);
+        $this->assertDatabaseMissing('message_templates', ['name' => 'Did not understand']);
+
+        Artisan::call(
+            'specification:import',
+            [
+                '--yes' => true
+            ]
+        );
+
+        $this->assertDatabaseHas('conversations', ['name' => 'no_match_conversation']);
+        $this->assertDatabaseHas('outgoing_intents', ['name' => 'intent.core.NoMatchResponse']);
+        $this->assertDatabaseHas('message_templates', ['name' => 'Did not understand']);
+    }
+
+    public function testExportSpecification()
+    {
+        $this->assertDatabaseMissing('conversations', ['name' => 'no_match_conversation']);
+        $this->assertDatabaseMissing('outgoing_intents', ['name' => 'intent.core.NoMatchResponse']);
+        $this->assertDatabaseMissing('message_templates', ['name' => 'Did not understand']);
+
+        Artisan::call(
+            'specification:import',
+            [
+                '--yes' => true
+            ]
+        );
+
+        $this->assertDatabaseHas('conversations', ['name' => 'no_match_conversation']);
+        $this->assertDatabaseHas('outgoing_intents', ['name' => 'intent.core.NoMatchResponse']);
+        $this->assertDatabaseHas('message_templates', ['name' => 'Did not understand']);
+
+        Artisan::call(
+            'specification:export',
+            [
+                '--yes' => true
+            ]
+        );
+    }
+}
