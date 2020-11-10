0bd556492d8a6b085580e9d7fbcc6d1d46892048
elliotmassen <elliotm@greenshootlabs.com>
2020-11-10 15:12:35 +0000

    Removing conversation & webchat setup commands from docker-update.sh

--- scripts/update-docker.sh
+++ scripts/update-docker.sh
@@ -5,12 +5,6 @@ set -e
 echo "Setting up the database..."
 php artisan migrate --force
 
-echo "Populating default webchat settings..."
-php artisan webchat:setup
-
-echo "Creating example conversations..."
-php artisan conversations:setup --non-interactive
-
 echo "Creating admin user"
 php artisan admin_user:create
 
