1d9ec8698206895c16831af4c8928aef7eafe953
elliotmassen <elliotm@greenshootlabs.com>
2020-12-10 08:57:34 +0000

    Removing Lando references from update-docker.sh and ensuring new commands are run in non-interactive mode

--- scripts/update-docker.sh
+++ scripts/update-docker.sh
@@ -9,13 +9,13 @@ echo "Populating default webchat settings..."
 php artisan webchat:setup
 
 echo "Initializing dgraph schema..."
-lando artisan schema:init
+php artisan schema:init --yes
 
 echo "Creating example conversations..."
 php artisan conversations:setup --non-interactive
 
 echo "Creating outgoing intents and message templates..."
-lando artisan messages:setup
+php artisan messages:import --yes
 
 echo "Creating admin user"
 php artisan admin_user:create
