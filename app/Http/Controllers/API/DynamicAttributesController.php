d31d930ebf3b5739e448a630d6d8bdcf57de4afe
Benjamin Dudiak-Fry <bend@deeson.co.uk>
2021-01-28 18:24:52 +0000

    Updated DynamicAttributes controller to work with updated DynamicAttribute model using integer primary key.

--- app/Http/Controllers/API/DynamicAttributesController.php
+++ app/Http/Controllers/API/DynamicAttributesController.php
@@ -26,6 +26,90 @@ class DynamicAttributesController extends Controller
         $this->middleware('auth');
     }
 
+    /**
+     * Upload a JSON object of { attribute_id: attribute_type } pairs
+     * as DynamicAttributes.
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public static function upload(Request $request): Response
+    {
+        $data = $request->all();
+        if ($error = self::validateImport($data)) {
+            return response($error, 400);
+        }
+
+        try {
+            DB::transaction(function () use ($data) {
+                foreach ($data as $id => $type) {
+                    DynamicAttribute::create(['attribute_id' => $id, 'attribute_type' => $type]);
+                }
+            });
+            return response($data, 201);
+        } catch (QueryException $e) {
+            return response(
+                "Unexpected occurred saving uploaded dynamic attributes.",
+                500
+            );
+        }
+    }
+
+    /**
+     * Validate uploaded DynamicAttributes
+     *
+     * @param array $data
+     *
+     * @return array|null
+     */
+    public static function validateImport(array $data): ?array
+    {
+        if (empty($data)) {
+            return [
+                'message' => 'No data in upload. Upload be a JSON object in of the form:
+ { <attribute_id>: attribute.<component>.<attribute_type> }',
+            ];
+        }
+
+        $invalidIds = array_filter(
+            array_keys($data),
+            fn($id) => !DynamicAttribute::isValidId($id)
+        );
+        if (!empty($invalidIds)) {
+            return [
+                'ids' => $invalidIds,
+                'message' => 'Invalid attribute ids in upload. All attribute Ids must be in snake_case',
+            ];
+        }
+
+        $invalidTypes = array_filter(
+            array_values($data),
+            fn($type) => !DynamicAttribute::isValidType($type)
+        );
+        if (!empty($invalidTypes)) {
+            return [
+                'types' => $invalidTypes,
+                'message' => 'Invalid attribute types in upload. All attribute types must be in the following format:
+ attribute.<component>.<type>',
+            ];
+        }
+
+        $ids = array_keys($data);
+        $existing = DynamicAttribute::whereIn('attribute_id', $ids);
+        /* Todo: Check for existing attributes in config */
+        if ($existing->count() > 0) {
+            return [
+                'ids' => $existing->get()->map(fn($item) => $item->id),
+                'message' => 'Some ids in upload are already in use.',
+            ];
+        }
+
+        $types = array_values($data);
+        // Todo: Check if the types exist.
+
+        return null;
+    }
 
     /**
      * Display a listing of the resource.
@@ -36,7 +120,6 @@ class DynamicAttributesController extends Controller
     {
         return new DynamicAttributeCollection(DynamicAttribute::paginate(50));
     }
-
 
     /**
      * Store a newly created resource in storage.
@@ -53,12 +136,13 @@ class DynamicAttributesController extends Controller
             return response($error, 400);
         }
 
-        if (DynamicAttribute::find($dynamicAttribute->id) || false /* TODO: Check for existing attributes from config */) {
+        if (DynamicAttribute::where('attribute_id', $dynamicAttribute->attribute_id)->exists()
+            || false /* TODO: Check for existing attributes from config */) {
             return response([
-                'field' => 'id',
+                'field' => 'attribute_id',
                 'message' => sprintf(
-                    "Id '%s' is already in use.",
-                    $dynamicAttribute->id
+                    "Attribute ID '%s' is already in use.",
+                    $dynamicAttribute->attribute_id
                 ),
             ], 400);
         }
@@ -68,8 +152,8 @@ class DynamicAttributesController extends Controller
         } catch (QueryException $e) {
             return response(
                 [
-                'attribute' => $dynamicAttribute->toJson(),
-                'message' => "Unexpected occurred saving dynamic attribute",
+                    'attribute' => $dynamicAttribute->toJson(),
+                    'message' => "Unexpected error occurred saving dynamic attribute",
                 ],
                 500
             );
@@ -85,34 +169,34 @@ class DynamicAttributesController extends Controller
      */
     private function validateValue(DynamicAttribute $dynamicAttribute): ?array
     {
-        if (empty($dynamicAttribute->id)) {
+        if (empty($dynamicAttribute->attribute_id)) {
             return [
-                'field' => 'id',
-                'message' => 'Attribute ID field is required.',
+                'field' => 'attribute_id',
+                'message' => 'attribute_id field is required.',
             ];
         }
 
-        if (empty($dynamicAttribute->type)) {
+        if (empty($dynamicAttribute->attribute_type)) {
             return [
-                'field' => 'type',
-                'message' => 'Type field is required.',
+                'field' => 'attribute_type',
+                'message' => 'attribute_type field is required.',
             ];
         }
 
-        if ($dynamicAttribute->id) {
-            if (!DynamicAttribute::isValidId($dynamicAttribute->id)) {
+        if ($dynamicAttribute->attribute_id) {
+            if (!DynamicAttribute::isValidId($dynamicAttribute->attribute_id)) {
                 return [
-                    'field' => 'id',
-                    'message' => 'ID field must follow snake_case format.',
+                    'field' => 'attribute_id',
+                    'message' => 'attribute_id field must follow snake_case format.',
                 ];
             }
         }
 
-        if ($dynamicAttribute->type) {
-            if (!DynamicAttribute::isValidType($dynamicAttribute->type)) {
+        if ($dynamicAttribute->attribute_type) {
+            if (!DynamicAttribute::isValidType($dynamicAttribute->attribute_type)) {
                 return [
-                    'field' => 'type',
-                    'message' => "Type field must follow the format: 'attribute.<component_name>.<type>'",
+                    'field' => 'attribute_type',
+                    'message' => "attribute_type field must follow the format: 'attribute.<component_name>.<type>'",
                 ];
             }
         }
@@ -127,11 +211,11 @@ class DynamicAttributesController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param string $id
+     * @param int $id
      *
      * @return DynamicAttributeResource
      */
-    public function show(string $id): DynamicAttributeResource
+    public function show(int $id): DynamicAttributeResource
     {
         if ($dynamicAttribute = DynamicAttribute::find($id)) {
             return new DynamicAttributeResource($dynamicAttribute);
@@ -144,27 +228,27 @@ class DynamicAttributesController extends Controller
      * Update the specified resource in storage.
      *
      * @param Request $request
-     * @param string $id
+     * @param int $id
      *
      * @return Response
      */
-    public function update(Request $request, string $id): Response
+    public function update(Request $request, int $id): Response
     {
         if ($dynamicAttribute = DynamicAttribute::find($id)) {
             $dynamicAttribute->fill($request->all());
 
-            /* If we're changing an existing DynamicAttribute's id, check for duplicates */
-            if ($dynamicAttribute->id !== $id) {
-                if (DynamicAttribute::find($dynamicAttribute->id)
-                    || false /* TODO: Check for existing attributes from config */) {
-                    return response([
-                        'field' => 'id',
-                        'message' => sprintf(
-                            "Id '%s' is already in use.",
-                            $dynamicAttribute->id
-                        ),
-                    ], 400);
-                }
+            $sameAttributeId = DynamicAttribute::where([
+                ['attribute_id', $dynamicAttribute->attribute_id],
+                ['id', '<>', $dynamicAttribute->id]
+            ])->get();
+            if ($sameAttributeId->count() > 0 || false /* TODO: Check for existing attributes from config */) {
+                return response([
+                    'field' => 'attribute_id',
+                    'message' => sprintf(
+                        "Attribute id '%s' is already in use.",
+                        $dynamicAttribute->attribute_id
+                    ),
+                ], 400);
             }
 
             if ($error = $this->validateValue($dynamicAttribute)) {
@@ -188,16 +272,16 @@ class DynamicAttributesController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param string $id
+     * @param int $id
      *
      * @return Response
      */
-    public function destroy(string $id): Response
+    public function destroy(int $id): Response
     {
         if ($dynamicAttribute = DynamicAttribute::find($id)) {
             try {
                 $dynamicAttribute->delete();
-                return response()->noContent(200);
+                return response()->noContent();
             } catch (Exception $e) {
                 return response(sprintf(
                     'Error deleting DynamicAttribute %s',
@@ -218,98 +302,12 @@ class DynamicAttributesController extends Controller
     {
         $map = [];
         foreach (DynamicAttribute::all() as $dynamicAttribute) {
-            $map[$dynamicAttribute->id] = $dynamicAttribute->type;
+            $map[$dynamicAttribute->attribute_id] = $dynamicAttribute->attribute_type;
         }
         return new JsonResponse(
             $map,
             200,
-            ['Content-Disposition' => 'attachment; filename="dynamic-attributes.json"']
+            ['Content-Disposition' => 'attachment; filename="custom-attributes.json"']
         );
-    }
-
-
-    /**
-     * Upload a JSON object of { attribute_id: attribute_type } pairs
-     * as DynamicAttributes.
-     *
-     * @param Request $request
-     *
-     * @return Response
-     */
-    public function upload(Request $request): Response
-    {
-        $data = $request->all();
-        if ($error = $this->validateUpload($data)) {
-            return response($error, 400);
-        }
-
-        try {
-            DB::transaction(function () use ($data) {
-                foreach ($data as $id => $type) {
-                    DynamicAttribute::create(['id' => $id, 'type' => $type]);
-                }
-            });
-            return response($data, 201);
-        } catch (QueryException $e) {
-            return response(
-                "Unexpected occurred saving uploaded dynamic attributes.",
-                500
-            );
-        }
-    }
-
-    /**
-     * Validate uploaded DynamicAttributes
-     *
-     * @param array $data
-     *
-     * @return array|null
-     */
-    public function validateUpload(array $data): ?array
-    {
-        if (empty($data)) {
-            return [
-                'message' => 'Unable to parse upload body. Uploads must be a JSON object in of the form:
- { <attribute_id>: attribute.<component>.<attribute_type> }',
-            ];
-        }
-
-        $invalidIds = array_filter(
-            array_keys($data),
-            fn ($id) => !DynamicAttribute::isValidId($id)
-        );
-        if (!empty($invalidIds)) {
-            return [
-                'ids' => $invalidIds,
-                'message' => 'Invalid attribute ids in upload. All attribute Ids must be in snake_case',
-            ];
-        }
-
-        $invalidTypes = array_filter(
-            array_values($data),
-            fn ($type) => !DynamicAttribute::isValidType($type)
-        );
-        if (!empty($invalidTypes)) {
-            return [
-                'types' => $invalidTypes,
-                'message' => 'Invalid attribute types in upload. All attribute types must be in the following format:
- attribute.<component>.<type>',
-            ];
-        }
-
-        $ids = array_keys($data);
-        $existing = DynamicAttribute::whereIn('id', $ids);
-        /* Todo: Check for existing attributes in config */
-        if ($existing->count() > 0) {
-            return [
-                'ids' => $existing->get()->map(fn ($item) => $item->id),
-                'message' => 'Some ids in upload are already in use.',
-            ];
-        }
-
-        $types = array_values($data);
-        // Todo: Check if the types exist.
-
-        return null;
     }
 }
