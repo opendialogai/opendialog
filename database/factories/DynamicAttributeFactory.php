0b856bb067b1f248a1dd5230157321e7859fb7b5
Benjamin Dudiak-Fry <bend@deeson.co.uk>
2021-02-05 15:13:35 +0000

    Updated DynamicAttributeFactory to use guaranteed registered attribute_type values by selecting from a list of core attribute types.

--- database/factories/DynamicAttributeFactory.php
+++ database/factories/DynamicAttributeFactory.php
@@ -10,6 +10,12 @@ use OpenDialogAi\AttributeEngine\DynamicAttribute;
 $factory->define(DynamicAttribute::class, function (Faker $faker) {
     return [
         'attribute_id' => $faker->unique()->regexify(AttributeResolver::getValidIdPattern()),
-        'attribute_type' => $faker->regexify(AttributeResolver::getValidTypePattern())
+        'attribute_type' => $faker->randomElement([
+            'attribute.core.int',
+            'attribute.core.array',
+            'attribute.core.boolean',
+            'attribute.core.string',
+            'attribute.core.timestamp'
+        ])
     ];
 });
