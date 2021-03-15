c9597abae3fa5a3b44ceb20316e507f9cba64cb6
Pat Shone <pats@greenshootlabs.com>
2021-03-15 12:11:06 +0000

    working test for focused conversation in UI state API

--- tests/Feature/UIStateControllerTest.php
+++ tests/Feature/UIStateControllerTest.php
@@ -7,9 +7,13 @@ use App\Http\Facades\Serializer;
 use App\Http\Resources\ConversationResource;
 use App\Http\Resources\ScenarioResource;
 use App\User;
+use Carbon\Carbon;
+use OpenDialogAi\Core\Conversation\BehaviorsCollection;
+use OpenDialogAi\Core\Conversation\ConditionCollection;
 use OpenDialogAi\Core\Conversation\Conversation;
 use OpenDialogAi\Core\Conversation\Exceptions\ConversationObjectNotFoundException;
 use OpenDialogAi\Core\Conversation\Facades\ConversationDataClient;
+use OpenDialogAi\Core\Conversation\Scenario;
 use Tests\TestCase;
 
 class UIStateControllerTest extends TestCase
@@ -40,34 +44,25 @@ class UIStateControllerTest extends TestCase
 
     public function testGetFocusedConversation()
     {
-        $this->markTestSkipped(
-            'Test in progress'
-        );
-        $fakeConversation = new Conversation();
-        $fakeConversation->setUid('0x001');
-        // What's the rest of the conversation I get back
+        $fakeScenario = new Scenario();
+        $fakeScenario->setUid('0x0001');
+        $fakeScenario->setName("Example scenario");
+        $fakeScenario->setOdId('example_scenario');
+        $fakeScenario->setDescription('An example scenario');
 
-//        Serializer::shouldReceive('normalize')
-//            ->once()
-//            ->with($fakeConversation, 'json', ConversationResource::$fields)
-//            ->andReturn(json_decode('{
-//            "id": "0x001",
-//            "od_id": "new_example_conversation",
-//            "name": "New Example conversaation",
-//            "description": "An new example conversation",
-//            "interpreter": "interpreter.core.nlp",
-//            "behaviors": [],
-//            "conditions": [],
-//            "created_at": "2021-03-12T11:57:23+0000",
-//            "updated_at": "2021-03-12T11:57:23+0000",
-//            "scenario": {
-//                "id": "0x8",
-//                "od_id": "simple_scenario another update",
-//                "name": "Simple Scenario updated again",
-//                "description": "A Simple Scenario Again"
-//            },
-//            "scenes": []
-//        }', true));
+        $fakeConversation = new Conversation();
+        $fakeConversation->setUid('0x0002');
+        $fakeConversation->setName('New Example conversation');
+        $fakeConversation->setOdId('new_example_conversation');
+        $fakeConversation->setDescription("An new example conversation");
+        $fakeConversation->setInterpreter('interpreter.core.nlp');
+        $fakeConversation->setBehaviors(new BehaviorsCollection());
+        $fakeConversation->setConditions(new ConditionCollection());
+        $fakeConversation->setCreatedAt(Carbon::parse('2021-03-12T11:57:23+0000'));
+        $fakeConversation->setUpdatedAt(Carbon::parse('2021-03-12T11:57:23+0000'));
+
+        $fakeConversation->setScenario($fakeScenario);
+
         ConversationDataClient::shouldReceive('getConversationByUid')
             ->once()
             ->with($fakeConversation->getUid(), false)
@@ -75,13 +70,29 @@ class UIStateControllerTest extends TestCase
 
         ConversationDataClient::shouldReceive('getScenarioWithFocusedConversation')
             ->once()
-            ->with($fakeConversation->getUid(), false)
+            ->with($fakeConversation->getUid())
             ->andReturn($fakeConversation);
 
         $this->actingAs($this->user, 'api')
             ->json('GET', '/admin/api/conversation-builder/ui-state/focused/conversation/' . $fakeConversation->getUid())
-            ->assertJson([
-
+            ->assertExactJson([
+                'scenario' => [
+                    'id'=> '0x0001',
+                    'od_id'=> 'example_scenario',
+                    'name'=> 'Example scenario',
+                    'description'=> 'An example scenario',
+                   'focusedConversation' => [
+                       "id" => "0x0002",
+                       "name" => "New Example conversation",
+                       "od_id" => "new_example_conversation",
+                       "description" => "An new example conversation",
+                       "interpreter" => "interpreter.core.nlp",
+                       "behaviors" => [],
+                       "conditions" => [],
+                       "created_at" => "2021-03-12T11:57:23+0000",
+                       "updated_at" => "2021-03-12T11:57:23+0000"
+                   ]
+                ]
             ]);
     }
 }
