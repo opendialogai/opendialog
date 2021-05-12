3dc606023b84ad2b549e891f1610e08c293a3610
Stuart Haigh <stuarth@greenshootlabs.com>
2021-05-12 10:14:55 +0100

    returns full conversation graph for message templates

--- app/Http/Resources/MessageTemplateGraphResource.php
+++ app/Http/Resources/MessageTemplateGraphResource.php
@@ -7,7 +7,11 @@ use App\Http\Facades\Serializer;
 use Illuminate\Http\Resources\Json\JsonResource;
 use OpenDialogAi\Core\Conversation\Behavior;
 use OpenDialogAi\Core\Conversation\Condition;
+use OpenDialogAi\Core\Conversation\Conversation;
+use OpenDialogAi\Core\Conversation\Intent;
 use OpenDialogAi\Core\Conversation\MessageTemplate;
+use OpenDialogAi\Core\Conversation\Scenario;
+use OpenDialogAi\Core\Conversation\Scene;
 use OpenDialogAi\Core\Conversation\Turn;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 
@@ -27,7 +31,29 @@ class MessageTemplateGraphResource extends JsonResource
             MessageTemplate::BEHAVIORS => Behavior::FIELDS,
             MessageTemplate::CONDITIONS => Condition::FIELDS,
             MessageTemplate::INTENT => [
-                Turn::UID
+                Intent::UID,
+                Intent::OD_ID,
+                Intent::NAME,
+                Intent::TURN => [
+                    Turn::UID,
+                    Turn::OD_ID,
+                    Turn::NAME,
+                    Turn::SCENE => [
+                        Scene::UID,
+                        Scene::OD_ID,
+                        Scene::NAME,
+                        Scene::CONVERSATION => [
+                            Conversation::UID,
+                            Conversation::OD_ID,
+                            Conversation::NAME,
+                            Conversation::SCENARIO => [
+                                Scenario::UID,
+                                Scenario::OD_ID,
+                                Scenario::NAME,
+                            ]
+                        ]
+                    ]
+                ]
             ]
         ]
     ];
