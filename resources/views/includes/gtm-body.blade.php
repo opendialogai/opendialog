47338e455a6cb383063ac66af110055390d46424
Stuart Haigh <stuarth@greenshootlabs.com>
2021-07-14 10:26:55 +0100

    updates gtm blade files to use config values rather than env to avoid issues if caching

--- resources/views/includes/gtm-body.blade.php
+++ resources/views/includes/gtm-body.blade.php
@@ -1,6 +1,6 @@
-@if (env("ENABLE_GTM"))
+@if (config('gtm.enable'))
     <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{ env("GTM_CONTAINER") }}&gtm_auth={{ env("GTM_AUTH") }}&gtm_preview={{ env("GTM_ENVIRONMENT") }}&gtm_cookies_win=x"
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{ config('gtm.container') }}&gtm_auth={{ config('gtm.auth') }}&gtm_preview={{ config('gtm.environment') }}&gtm_cookies_win=x"
                       height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
 @endif
