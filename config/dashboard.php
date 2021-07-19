98f1ff1f30a432783008719cd97d920d0849bdfb
elliotmassen <elliotm@greenshootlabs.com>
2021-07-19 15:46:02 +0100

    Ensuring that dashboard notification endpoint is defaulted in the config file

--- config/dashboard.php
+++ config/dashboard.php
@@ -2,5 +2,5 @@
 
 return [
     'version' => env("DASHBOARD_VERSION", 'Version 1.0.0'),
-    'notifications_endpoint' => env("DASHBOARD_NOTIFICATIONS_ENDPOINT", ''),
+    'notifications_endpoint' => env("DASHBOARD_NOTIFICATIONS_ENDPOINT", 'https://notifications.opendialog.ai/os.json'),
 ];
