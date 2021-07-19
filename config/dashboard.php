b5124897e3131235976c93a9c66f5a5bf0efcd2e
elliotmassen <elliotm@greenshootlabs.com>
2021-07-19 13:35:11 +0100

    Ensuring that dashboard version is defaulted in the config file

--- config/dashboard.php
+++ config/dashboard.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'version' => env("DASHBOARD_VERSION", ''),
+    'version' => env("DASHBOARD_VERSION", 'Version 1.0.0'),
     'notifications_endpoint' => env("DASHBOARD_NOTIFICATIONS_ENDPOINT", ''),
 ];
